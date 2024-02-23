05bfcd886e2b48c3d28391901452b53326504d13
Adigamahoo <mohammed.adiga011@gmail.com>
2024-02-23 21:31:05 +0300

    Define website home page

---
+++ resources/js/Components/app/FollowingListItems.vue
@@ -0,0 +1,43 @@
+<script setup>
+    import TextInput from '@/Components/TextInput.vue';
+    import {ref} from 'vue';
+
+    import FollowingItem from './FollowingItem.vue';   
+</script>
+
+<template>
+    <TextInput :model-value="searchKeyword" placeholder="Type to search" class="w-full mt-4"/>
+
+    <div class="mt-3 h-[200px] lg:flex-1 overflow-auto">
+
+        <div v-if="false" class="text-gray-400 text-center p-3" >
+            You don't have friends yet.
+        </div>
+
+        <div v-else>
+            <FollowingItem image="https://picsum.photos/100" title="joun Doe" />
+            <FollowingItem image="https://picsum.photos/200" title="Elon Musk" />
+            <FollowingItem image="https://picsum.photos/100" title="joun Doe" />
+            <FollowingItem image="https://picsum.photos/200" title="Elon Musk" />
+            <FollowingItem image="https://picsum.photos/100" title="joun Doe" />
+            <FollowingItem image="https://picsum.photos/200" title="Elon Musk" />
+            <FollowingItem image="https://picsum.photos/100" title="joun Doe" />
+            <FollowingItem image="https://picsum.photos/200" title="Elon Musk" />
+            <FollowingItem image="https://picsum.photos/100" title="joun Doe" />
+            <FollowingItem image="https://picsum.photos/200" title="Elon Musk" />
+            <FollowingItem image="https://picsum.photos/100" title="joun Doe" />
+            <FollowingItem image="https://picsum.photos/200" title="Elon Musk" />
+            <FollowingItem image="https://picsum.photos/100" title="joun Doe" />
+            <FollowingItem image="https://picsum.photos/200" title="Elon Musk" />
+            <FollowingItem image="https://picsum.photos/100" title="joun Doe" />
+            <FollowingItem image="https://picsum.photos/200" title="Elon Musk" />
+            <FollowingItem image="https://picsum.photos/100" title="joun Doe" />
+            <FollowingItem image="https://picsum.photos/200" title="Elon Musk" />
+        </div>
+    </div>
+</template>
+
+
+<style>
+    
+</style>
---
+++ resources/js/Components/app/PostList.vue
@@ -0,0 +1,65 @@
+<script setup>
+import PostItem from './PostItem.vue';
+
+const post1= {
+    user: {
+        id: 1,
+        avatar: 'https://randomuser.me/api/portraits/men/72.jpg',
+        name: 'John Smith',
+    },
+    group: null,
+    attachments: [
+        {
+            id: 1,
+            name: 'test.png',
+            url: 'https://picsum.photos/1000',
+            mime: 'image/png',
+        },
+        {
+            id: 2,
+            name: 'test2.png',
+            url: 'https://picsum.photos/1000',
+            mime: 'image/png',
+        },
+        {
+            id: 3,
+            name: 'MyDocument.docx',
+            url: '#',
+            mime: 'application/msword',
+        }
+    ],
+    body:`<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis neque ex harum quis explicabo exercitationem, impedit quisquam ab praesentium possimus voluptatem animi. Rem et deserunt excepturi beatae, sint suscipit corporis!</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis neque ex harum quis explicabo exercitationem, impedit quisquam ab praesentium possimus voluptatem animi. Rem et deserunt excepturi beatae, sint suscipit corporis!</p>`,
+    create_at: '2024-11-19 15:12',
+}
+
+const post2= {
+    user: {
+        id: 1,
+        avatar: 'https://randomuser.me/api/portraits/men/36.jpg',
+        name: 'Mohammed Younes',
+    },
+    group: {
+        id: 1,
+        name: 'Laravel Developers',
+    },
+    body:`<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis neque ex harum quis explicabo exercitationem, impedit quisquam ab praesentium possimus voluptatem animi. Rem et deserunt excepturi beatae, sint suscipit corporis!</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis neque ex harum quis explicabo exercitationem, impedit quisquam ab praesentium possimus voluptatem animi. Rem et deserunt excepturi beatae, sint suscipit corporis!</p>`,
+    create_at: '2022-07-19 01:15',
+}
+
+</script>
+
+
+<template>
+    <div class="overflow-auto">
+        <PostItem :post="post1"/>
+        <PostItem :post="post2"/>
+        <PostItem :post="post1"/>
+    </div>
+</template>
+
+<style>
+    
+</style>
+
